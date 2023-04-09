1bec27b873102cb8833775afe145bed00489f2cf
ismail <ismail.syedy@gmail.com>
2023-04-09 04:33:55 -0600

    Completed

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+// remove element 
+const removeMain = document.getElementById("main")
+removeMain.remove()
+
+// adding element 
+const newHeader = document.createElement("h1")
+newHeader.id = 'victory'
+newHeader.innerHTML = "ismail is the champion"
